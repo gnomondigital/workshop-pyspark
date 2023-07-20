d7672093ac0ae1ffd3108781fce46ff50467607b
Ines Ben Ameur <ines.benameur@gnomondigital.com>
2023-07-20 17:24:50 +0200

    add unit tests

--- urban_consumption/tests/conftest.py
+++ urban_consumption/tests/conftest.py
@@ -14,20 +14,16 @@ def init_spark_session():
 def generate_data(init_spark):
     """Generate data for tests"""
     data = [
-        (1, 2007, 2),
-        (1, 2008, 4),
-        (1, 2012, 6),
-        (2, 2007, 1),
-        (2, 2008, 2),
-        (2, 2012, 3),
+        (1, 2007, 2, 10, 100),
+        (1, 2008, 4, 20, 100),
+        (1, 2012, 6, 30, 300),
+        (2, 2007, 1, 10, 100),
+        (2, 2008, 2, 20, 100),
+        (2, 2012, 3, 30, 300),
     ]
     data_df = init_spark.createDataFrame(
         data,
-        [
-            "Entity",
-            "Year",
-            "Oil_production_(Etemad_&_Luciana)_(terawatt-hours)_per_capita",
-        ],
+        ["Entity", "Year", "meat_prod_tonnes_per_capita", "col_1", "Population"],
     )
     data_df.show()
     yield data_df
--- urban_consumption/tests/test_functions.py
+++ urban_consumption/tests/test_functions.py
@@ -1,6 +1,7 @@
 import pytest
 from pyspark.sql import DataFrame
 import pyspark.sql.functions as F
+from pyspark_test import assert_pyspark_df_equal
 import urban_consumption.functions as f
 
 
@@ -9,3 +10,32 @@ def test_filter_dates(test_data: DataFrame):
     result = f.filter_dates(test_data)
     result = result.filter((F.col("Year") < 2008) | (F.col("Year") > 2012))
     assert result.count() == 0
+
+
+def test_convert_to_per_capita(init_spark, test_data: DataFrame):
+    """Check that data was filtered according to the required range"""
+    data = [
+        (0.1,),
+        (0.2,),
+        (0.1,),
+        (0.1,),
+        (0.2,),
+        (0.1,),
+    ]
+    expected = init_spark.createDataFrame(
+        data,
+        ["col_1_per_capita"],
+    )
+    result = f.convert_to_per_capita(test_data, ["col_1"])
+    assert "col_1_per_capita" in expected.columns
+    assert_pyspark_df_equal(expected, result.select("col_1_per_capita"))
+
+
+def test_country_with_highest_avg_meat(test_data: DataFrame):
+    """Test the country_with_highest_avg_meat function"""
+    avg_expected = test_data.groupBy("Entity").agg(
+        F.mean("meat_prod_tonnes_per_capita").alias("meat_prod_tonnes_per_capita")
+    )
+    avg_expected.show()
+    expected_count = f.country_with_highest_avg_meat(avg_expected)
+    assert expected_count == 1
